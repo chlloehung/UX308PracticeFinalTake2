cad70124970a92534ce8f9424ad045f0c8bba487
chlloehung <144968987+chlloehung@users.noreply.github.com>
2025-04-17 01:21:11 +0000

    five questions complete

--- src/components.jsx
+++ src/components.jsx
@@ -1,23 +1,53 @@
-import {fahrenheitToCelsius, hello} from './functions.js';
+import {colourMix, largestProduct, dayOfTheWeek, payRaise, isLeap} from './functions.js';
 
 function Question1(){
     return <section>
-1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
-      <h2>results</h2>
-      <p>hello("Rich") == "{hello('Rich')}"</p>
-      <p>hello("Bill") == "{hello('Bill')}"</p>
-      <p>hello("Chris") == "{hello('Chris')}"</p>
+1. Determine the secondary RGB colour from mixing two primary RGB colours. 
+     <h2>results</h2>
+     <p>colourMix("blue", "red") == "{colourMix('blue', 'red')}"</p>     
+     <p>colourMix("blue", "blue") == "{colourMix('blue', 'blue')}"</p> 
+     <p>colourMix("green", "red") == "{colourMix('green', 'red')}"</p>      
     </section>;
 }
 
 function Question2(){
     return <section>
-2. Write a function that converts from fahrenheit to celsius        
-<h2>results</h2>
-<p>fahrenheitToCelsius(32) == "{fahrenheitToCelsius(32)}"</p>
-<p>fahrenheitToCelsius(212) == "{fahrenheitToCelsius(212)}"</p>
-<p>fahrenheitToCelsius(70) == "{fahrenheitToCelsius(70)}"</p>
-    </section>
+2. Return the product of the two largest values out of three numbers.
+     <h2>results</h2>
+     <p>largestProduct(3, 7, 10) == "{largestProduct(3, 7, 10)}"</p>   
+     <p>largestProduct(15, -5, 2) == "{largestProduct(15, -5, 2)}"</p>         
+     <p>largestProduct(6, 6, 9) == "{largestProduct(6, 6, 9)}"</p>      
+    </section>;
 }
 
-export {Question1, Question2}
+function Question3(){
+    return <section>
+3. Return the name of a day of the week given an integer day number.   
+     <h2>results</h2>
+     <p>dayOfTheWeek(2) == "{dayOfTheWeek(2)}"</p>   
+     <p>dayOfTheWeek(6) == "{dayOfTheWeek(6)}"</p>   
+     <p>dayOfTheWeek(9) == "{dayOfTheWeek(9)}"</p>  
+    </section>;
+}
+
+function Question4(){
+    return <section>
+4. Calculate pay raises for employees.
+     <h2>results</h2>
+     <p>payRaise('F', 3, 10000) == "{payRaise('F', 3, 10000)}"</p>
+     <p>payRaise('P', 11, 15000) == "{payRaise('P', 11, 15000)}"</p>
+     <p>payRaise('F', 6, 40000) == "{payRaise('F', 6, 40000)}"</p>
+    </section>;
+}
+
+function Question5(){
+    return <section>
+5. Determine if a year is a leap year.
+     <h2>results</h2>
+     <p>isLeap(4) == "{isLeap(4).toString()}"</p>      
+     <p>isLeap(100) == "{isLeap(100).toString()}"</p>   
+     <p>isLeap(2024) == "{isLeap(2024).toString()}"</p>  
+   </section>;
+}
+
+export {Question1, Question2, Question3, Question4, Question5}
--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,117 @@
-function hello(name){
-    return(`hello ${name}`);
+function colourMix(rgb_colour1, rgb_colour2){
+    if (rgb_colour1 === "red" && rgb_colour2 === "blue"){
+        return "fuchsia";
+    }
+    if (rgb_colour1 === "blue" && rgb_colour2 === "red"){
+        return "fuchsia";
+    }
+    if (rgb_colour1 === "red" && rgb_colour2 === "green"){
+        return "yellow";
+    }
+    if (rgb_colour1 === "green" && rgb_colour2 === "red"){
+        return "yellow";
+    }
+    if (rgb_colour1 === "green" && rgb_colour2 === "blue"){
+        return "aqua";
+    }
+    if (rgb_colour1 === "blue" && rgb_colour2 === "green"){
+        return "aqua";
+    }
+    if (rgb_colour1 === "red" && rgb_colour2 === "red"){
+        return "red";
+    }
+    if (rgb_colour1 === "blue" && rgb_colour2 === "blue"){
+        return "blue";
+    }
+    if (rgb_colour1 === "green" && rgb_colour2 === "green"){
+        return "green";
+    }
+    return "Error";
 }
 
-function fahrenheitToCelsius(fahrenheit){
-    const celsius = (fahrenheit -32) * 5/9
-    return celsius;
+function largestProduct(val1, val2, val3){
+    if (val1 < val2) {
+        if (val1 < val3) {
+            return val2 * val3;
+        }
+    }
+
+    if (val2 < val1) {
+        if (val2 < val3) {
+            return val1 * val3;
+        }
+    }
+
+    if (val3 < val1) {
+        if (val3 < val2) {
+            return val1 * val2;
+        }
+    }
+
+    return val1 * val2;
 }
 
-export {hello, fahrenheitToCelsius}
+function dayOfTheWeek(day_num){
+    if (day_num === 1) {
+        return "Sunday";
+    }
+    if (day_num === 2) {
+        return "Monday";
+    }
+    if (day_num === 3) {
+        return "Tuesday";
+    }
+    if (day_num === 4) {
+        return "Wednesday";
+    }
+    if (day_num === 5) {
+        return "Thursday";
+    }
+    if (day_num === 6) {
+        return "Friday";
+    }
+    if (day_num === 7) {
+        return "Saturday";
+    }
+    return "Error";
+}
+
+function payRaise(status, years, salary){
+    if (status === 'F') {
+        if (years >= 10) {
+            return salary + (salary * 0.05);
+        }
+        if (years < 4) {
+            return salary + (salary * 0.015);
+        }
+    }
+
+    if (status === 'P') {
+        if (years > 10) {
+            return salary + (salary * 0.03);
+        }
+        if (years < 4) {
+            return salary + (salary * 0.01);
+        }
+    }
+
+    return salary + (salary * 0.02);
+}
+
+function isLeap(year){
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+export {colourMix, largestProduct, dayOfTheWeek, payRaise, isLeap}
